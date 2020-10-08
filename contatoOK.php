3ad9ac3aa0c7b12e6b83e6e0f2b7388cc50bb83b
Antônio Neto <antonio.silva6@yahoo.com.br>
2020-10-08 09:01:04 -0300

    Adicionei a tag do google analytics

--- contatoOK.php
+++ contatoOK.php
@@ -16,6 +16,16 @@
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="style.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180089512-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-180089512-1');
+    </script>
 	
 	<script type="text/javascript">
 		var piscando = document.getElementById('whatsapp');
